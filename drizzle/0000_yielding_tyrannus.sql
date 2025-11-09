88787f56f5d314038254d2cfc9e23e48feea9416
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-11-09 07:17:20 +0000

    feat: add drizzle directory

---
+++ drizzle/0000_yielding_tyrannus.sql
@@ -0,0 +1,71 @@
+CREATE TABLE `account` (
+	`id` text PRIMARY KEY NOT NULL,
+	`account_id` text NOT NULL,
+	`provider_id` text NOT NULL,
+	`user_id` text NOT NULL,
+	`access_token` text,
+	`refresh_token` text,
+	`id_token` text,
+	`access_token_expires_at` integer,
+	`refresh_token_expires_at` integer,
+	`scope` text,
+	`password` text,
+	`created_at` integer DEFAULT (current_timestamp) NOT NULL,
+	`updated_at` integer DEFAULT (current_timestamp) NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `comments` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`post_id` integer NOT NULL,
+	`author` text NOT NULL,
+	`comment` text NOT NULL,
+	`created_at` integer DEFAULT (current_timestamp) NOT NULL,
+	`updated_at` integer DEFAULT (current_timestamp) NOT NULL,
+	FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `posts` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`author_id` text NOT NULL,
+	`title` text NOT NULL,
+	`slug` text NOT NULL,
+	`content` text,
+	`likes` integer DEFAULT 0,
+	`created_at` integer DEFAULT (current_timestamp) NOT NULL,
+	`updated_at` integer DEFAULT (current_timestamp) NOT NULL,
+	FOREIGN KEY (`author_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `session` (
+	`id` text PRIMARY KEY NOT NULL,
+	`expires_at` integer NOT NULL,
+	`token` text NOT NULL,
+	`ip_address` text,
+	`user_agent` text,
+	`user_id` text NOT NULL,
+	`created_at` integer DEFAULT (current_timestamp) NOT NULL,
+	`updated_at` integer DEFAULT (current_timestamp) NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `session_token_unique` ON `session` (`token`);--> statement-breakpoint
+CREATE TABLE `user` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`email_verified` integer DEFAULT false NOT NULL,
+	`image` text,
+	`created_at` integer DEFAULT (current_timestamp) NOT NULL,
+	`updated_at` integer DEFAULT (current_timestamp) NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);--> statement-breakpoint
+CREATE TABLE `verification` (
+	`id` text PRIMARY KEY NOT NULL,
+	`identifier` text NOT NULL,
+	`value` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	`created_at` integer DEFAULT (current_timestamp) NOT NULL,
+	`updated_at` integer DEFAULT (current_timestamp) NOT NULL
+);
